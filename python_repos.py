6762f2594b136ccccb93226dd590636cb591a43c
Paul Prete <53125076+paulpprete@users.noreply.github.com>
2019-11-05 20:47:48 -0500

    Add files via upload

---
+++ python_repos.py
@@ -0,0 +1,29 @@
+import requests
+import pygal
+
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
+#Make API, store response
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+
+r = requests.get(url)
+print("Status code:", r.status_code)
+
+#Storing API response in variable
+response_dict = r.json()
+
+#Process results
+print('Total repositories:', response_dict['total_count'])
+
+#Xploring information about repositories
+repo_dicts = response_dict['items']
+print('Repositories returned:', len(repo_dicts))
+
+print('\nSelected information about each repository:')
+for repo_dict in repo_dicts:	
+	print('\nName:', repo_dict['name']
+	print('Owner:', repo_dict['owner']['login'])
+	print('Stars:', repo_dict['stargazers_count'])
+	print('Repository:', repo_dict['html_url'])
+	print('Description:', repo_dict['description'])
